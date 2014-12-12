1ef325d2a1c7214973e418041d9b9b21cca4eeb4
Paul Hayes <fofr@users.noreply.github.com>
2014-12-12 11:04:11 +0000

    Create minimal sinatra dashboard app
    Based on https://github.com/edds/display-screen

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-publisher-display-screen
-========================
+# Publisher Display Dashboard
 
-Dashboard for GOV.UK admin tools
+Based on [display-screen](https://github.com/edds/display-screen) by @edds.
+
+To run the server you will need the following `ENV` variables set:
+
+```
+CLIENT_ID
+CLIENT_SECRET
+REFRESH_TOKEN
+```
+
+You can create the client id and client secret using the [Google developer
+console][1]. You need to generate the refresh token, there is a [short Ruby
+script to create one for you][2].
+
+Once you have them you can start the server running:
+
+```
+CLIENT_ID=... CLIENT_SECRET=... REFRESH_TOKEN=... ruby ./server.rb
+```
+
+You can then browse to the server in your browser.
+
+[1]: https://developer.google.com/console
+[2]: https://gist.github.com/edds/9363713
---
+++ server.rb
@@ -0,0 +1,53 @@
+require 'rubygems'
+require 'sinatra'
+require 'json'
+require 'rack-cache'
+require 'net/http'
+require 'net/https'
+require 'active_support/core_ext/hash'
+require 'active_support/core_ext/object'
+
+use Rack::Cache
+set :public_folder, 'public'
+set :bind, '0.0.0.0'
+
+if ENV['USERNAME'] && ENV['PASSWORD']
+  use Rack::Auth::Basic, 'Demo area' do |user, pass|
+    user == ENV['USERNAME'] && pass = ENV['PASSWORD']
+  end
+end
+
+get '/' do
+  File.read(File.join('public', 'index.html'))
+end
+
+get '/realtime' do
+  cache_control :public, :max_age => 20
+  query = { :access_token => get_token }.merge(params)
+
+  http = Net::HTTP.new('www.googleapis.com', 443)
+  http.use_ssl = true
+  req = Net::HTTP::Get.new("/analytics/v3alpha/data/realtime?#{query.to_param}")
+  response = http.request(req)
+  response.body
+end
+
+def get_token
+  if @token.nil? || @token_timeout < Time.now
+    params = {
+      'client_id' => ENV['CLIENT_ID'],
+      'client_secret' => ENV['CLIENT_SECRET'],
+      'refresh_token' => ENV['REFRESH_TOKEN'],
+      'grant_type' => 'refresh_token'
+    }
+    http = Net::HTTP.new('accounts.google.com', 443)
+    http.use_ssl = true
+    req = Net::HTTP::Post.new('/o/oauth2/token')
+    req.form_data = params
+    response = http.request(req)
+    data = JSON.parse(response.body)
+    @token_timeout = Time.now + data["expires_in"]
+    @token = data["access_token"]
+  end
+  @token
+end
